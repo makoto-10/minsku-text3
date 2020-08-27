506339fecf9a023b4c0f20d1a8796a91601e1593
makoto-10 <1027.mako.000@gmail.com>
2020-08-28 07:30:16 +0900

    みんすく テキスト3

---
+++ ch07/calcForm1.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+		<title>足し算をやろう！</title>
+	</head>
+	<body>
+		<form action="useFunction1.php" method="POST">
+			1つ目の数値：<input type="text" name="num1" value=""><br>
+			2つ目の数値：<input type="text" name="num2" value=""><br>
+			<input type="submit" value="計算">
+		</form>
+	</body>
+</html>
+
---
+++ ch07/useFunction1.php
@@ -0,0 +1,24 @@
+<?php
+    //引数に2つの数値を受け取り、その数値の足し算結果を
+    //戻り値として返します。
+    function calc($num1,$num2){
+        return $num1+$num2;
+    }
+
+    //画面からの入力データを取得する
+    $num1=$_POST['num1'];
+    $num2=$_POST['num2'];
+
+    //calc関数を呼び出し結果を受け取る
+    $result=calc($num1, $num2);
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+	</head>
+	<body>
+		■計算結果<br>
+		<?=$num1."+".$num2."=".$result?>
+	</body>
+</html>
+
